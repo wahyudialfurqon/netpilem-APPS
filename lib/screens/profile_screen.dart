96276ad8c0a5735bbafd4dea0c81c24603e27efc
wahyudialfurqon <w.alfurqon11@gmail.com>
2025-03-08 18:38:36 +0700

    toggle & set default button

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:Netpilem/models/movie.dart';
 import 'package:Netpilem/screens/detail_screen.dart';
+import 'package:Netpilem/screens/widgets/favorite_grid.dart';
 import 'package:Netpilem/services/api_services.dart';
 import 'package:flutter/material.dart';
 
@@ -14,12 +15,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
   List<Movie> favoriteMovies = [];
   final ApiServices apiServices = ApiServices();
   Map<int, bool> favoriteStatus = {};
+  bool isFavoriteGrid = false;
+  bool isSaveGrid = false;
+  bool isHistoryGrid = false;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _loadFavoriteMovies();
+    isHistoryGrid = true;
   }
 
   Future<void> _toggleFavorite(int movieId) async {
@@ -49,6 +54,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
       favoriteMovies = favoriteList;
     });
   }
+
+ void _setActiveGrid(String gridType){
+  setState((){
+    isHistoryGrid = gridType == "history";
+    isFavoriteGrid = gridType == "favorite";
+    isSaveGrid = gridType == "save";
+  });
+ }
 
   @override
   Widget build(BuildContext context) {
@@ -195,22 +208,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(
-                      Icons.history, // Nama ikon
-                      color: Colors.white, // Warna ikon
-                      size: 20, // Ukuran ikon
+                    IconButton(
+                      icon: Icon(
+                        Icons.history,
+                        color: isHistoryGrid ? const Color.fromARGB(255, 255, 17, 0) : Colors.white,
+                      ),
+                      onPressed: () => _setActiveGrid("history"),
                     ),
                     const SizedBox(width: 120),
-                    Icon(
-                      Icons.favorite, // Nama ikon
-                      color: Colors.white, // Warna ikon
-                      size: 20, // Ukuran ikon
+                    IconButton(
+                      icon: Icon(
+                        Icons.favorite,
+                        color: isFavoriteGrid ? const Color.fromARGB(255, 255, 17, 0) : Colors.white,
+                      ),
+                      onPressed: () => _setActiveGrid("favorite"),
                     ),
                     const SizedBox(width: 120),
-                    Icon(
-                      Icons.bookmark, // Nama ikon
-                      color: Colors.white, // Warna ikon
-                      size: 20, // Ukuran ikon
+                     IconButton(
+                      icon: Icon(
+                        Icons.bookmark,
+                        color: isSaveGrid ? const Color.fromARGB(255, 255, 17, 0) : Colors.white,
+                      ),
+                      onPressed: () => _setActiveGrid("save"),
                     ),
                   ],
                 ),
@@ -237,83 +256,26 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   ),
                 ),
                 //
-                //Disini
-                //
-                SizedBox(height: 10),
-                Expanded(
-                  child:
-                      favoriteMovies.isEmpty
-                          ? Center(
-                            child: Text(
-                              'No Favorite Here!',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          )
-                          : GridView.builder(
-                            shrinkWrap: true,
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  childAspectRatio: 0.8,
-                                ),
-                            itemCount: favoriteMovies.length,
-                            itemBuilder: (context, index) {
-                              final movie = favoriteMovies[index];
-                              return ClipRRect(
-                                child: GestureDetector(
-                                  onTap:
-                                      () => Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder:
-                                              (context) =>
-                                                  DetailScreens(movie: movie),
-                                        ),
-                                      ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 0, right:  20, left: 20),
-                                    child: AspectRatio(
-                                      aspectRatio: 200 / 260, 
-                                      child: Stack(
-                                        children: [
-                                          ClipRRect(
-                                            borderRadius: BorderRadius.circular(
-                                              10,
-                                            ),
-                                            child: Image.network(
-                                              'https://image.tmdb.org/t/p/w500/${movie.posterPath}',
-                                              width: 200,
-                                              height: 260,
-                                              fit: BoxFit.cover,
-                                            ),
-                                          ),
-                                          Positioned(
-                                            bottom: 35, 
-                                            right: 0,
-                                            child: IconButton(
-                                              icon: Icon(
-                                                favoriteStatus[movie.id] ?? true
-                                                    ? Icons.favorite
-                                                    : Icons.favorite_border,
-                                                color: Colors.red,
-                                                size: 30,
-                                              ),
-                                              onPressed:
-                                                  () => _toggleFavorite(movie.id),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                ),
+                const SizedBox(height: 10),
+                if (isFavoriteGrid)
+                  FavoriteGrid(
+                    favoriteMovies: favoriteMovies,
+                    favoriteStatus: favoriteStatus,
+                    onToggleFavorite: _toggleFavorite,
+                  )
+                else if (isSaveGrid)
+                  FavoriteGrid(
+                    favoriteMovies: favoriteMovies,
+                    favoriteStatus: favoriteStatus,
+                    onToggleFavorite: _toggleFavorite,
+                  )
+                else if (isHistoryGrid)
+                  FavoriteGrid(
+                    favoriteMovies: favoriteMovies,
+                    favoriteStatus: favoriteStatus,
+                    onToggleFavorite: _toggleFavorite,
+                  ),
+                //hilang
                 Padding(padding: EdgeInsets.only(top: 20)),
               ],
             ),
---
+++ lib/screens/widgets/favorite_grid.dart
@@ -0,0 +1,86 @@
+import 'package:Netpilem/models/movie.dart';
+import 'package:Netpilem/screens/detail_screen.dart';
+import 'package:flutter/material.dart';
+
+class FavoriteGrid extends StatelessWidget {
+  final List<Movie> favoriteMovies;
+  final Map<int, bool> favoriteStatus;
+  final Function(int) onToggleFavorite;
+
+  const FavoriteGrid({
+    Key? key,
+    required this.favoriteMovies,
+    required this.favoriteStatus,
+    required this.onToggleFavorite,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: favoriteMovies.isEmpty
+          ? const Center(
+              child: Text(
+                'No Favorite Here!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          : GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.8,
+              ),
+              itemCount: favoriteMovies.length,
+              itemBuilder: (context, index) {
+                final movie = favoriteMovies[index];
+                return ClipRRect(
+                  child: GestureDetector(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailScreens(movie: movie),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 0, right: 20, left: 20),
+                      child: AspectRatio(
+                        aspectRatio: 200 / 260,
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                'https://image.tmdb.org/t/p/w500/${movie.posterPath}',
+                                width: 200,
+                                height: 260,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 30,
+                              right: 0,
+                              child: IconButton(
+                                icon: Icon(
+                                  favoriteStatus[movie.id] ?? true
+                                      ? Icons.favorite
+                                      : Icons.favorite_border,
+                                  color: const Color.fromARGB(255, 255, 17, 0),
+                                  size: 30,
+                                ),
+                                onPressed: () => onToggleFavorite(movie.id),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
